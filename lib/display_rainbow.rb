fa917c6f3fdffe070b25193626ce71feb079467c
Muneeb Dahab <muneeb.dahab@outlook.com>
2020-11-26 03:06:15 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
 def 
-  print_display_rainbow(colors)
+  display_rainbow(colors)
 end
 def 
-  print_display_rainbow(colors)
+  display_rainbow(colors)
   puts "R: #{colors[0]},O: #{colors[1]},Y: #{colors[2]},G: #{colors[3]},B: #{colors[4]},I: #{colors[5]},V: #{colors[6]}"
 end
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-def print_display_rainbow(colors)
+def display_rainbow(colors)
   puts "R: #{colors[0]},O: #{colors[1]},Y: #{colors[2]},G: #{colors[3]},B: #{colors[4]},I: #{colors[5]},V: #{colors[6]}"
 end
